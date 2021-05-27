4d31c67a97d8651ab660d075066aa5b56bedafe2
deliaygambino <deliaygambino@hotmail.com>
2021-05-27 23:40:22 +0000

    Controller Added

---
+++ src/mic-book-galery-definitivo/Controllers/ComicBookController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mic_book_galery_definitivo.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            string todaysDate = DateTime.Now.ToShortDateString();
+            return Ok(todaysDate);
+        }
+    }
+}
