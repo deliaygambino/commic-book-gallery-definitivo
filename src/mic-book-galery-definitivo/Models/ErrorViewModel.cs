fb0e4883d73d6d0e6966e343280928344c397c28
deliaygambino <deliaygambino@hotmail.com>
2021-05-27 04:51:06 +0000

    initial commit

---
+++ src/mic-book-galery-definitivo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mic_book_galery_definitivo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
